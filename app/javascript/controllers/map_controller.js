cfdbd584c035a0a79d0d7dfad960991c00dcc7dd
Karina <karinasokolova252@gmail.com>
2025-11-04 14:15:34 +0200

    Add view polygon on map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,14 +4,33 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = { tickets: Array }
   connect() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    if(!this.ticketsValue || this.ticketsValue.length === 0){
+      this.element.textContent = "No any data"
+      return
+    }
+
+    const wkt = this.ticketsValue[0].well_known_text
+
+    const pairs = wkt
+      .replace('POLYGON((','')
+      .replace('))', '')
+      .split(',');
+
+    const latlngs = pairs.map(pair => {
+      const[lng, lat] = pair.trim().split(' ').map(Number)
+      return[lat, lng]
+    })
+
+    this.map = L.map(this.element)
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    L.marker([51.5, -0.09]).addTo(this.map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
+     var polygon = L.polygon(latlngs, {color: 'blue', fillColor: '#2859ad', fillOpacity: 0.5})
+
+     polygon.addTo(this.map)
+
+     this.map.fitBounds(polygon.getBounds())
   }
 }
