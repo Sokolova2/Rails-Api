9be03cf4cfec46666b18ac4b6bbd89614141c835
Karina <karinasokolova252@gmail.com>
2025-11-04 14:15:52 +0200

    Fix rubocop's remarks

--- app/helpers/api/tickets_helper.rb
+++ app/helpers/api/tickets_helper.rb
@@ -1,2 +1,6 @@
-module Api::TicketsHelper
+# frozen_string_literal: true
+
+module Api
+  module TicketsHelper
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'api/tickets#index'
 
   namespace :api do
-    resources :tickets, only: [ :index, :create ]
+    resources :tickets, only: %i[index create]
   end
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
