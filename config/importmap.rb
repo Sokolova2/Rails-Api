5ba9d421307b08fd1ff52e088a2c3959babc036c
Karina <karinasokolova252@gmail.com>
2025-11-04 14:36:38 +0200

    Fix rubocop's remarks

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# Pin npm packages by running ./bin/importmap
-
 pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
